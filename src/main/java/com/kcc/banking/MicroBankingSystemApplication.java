4df34d633e5b1727bec9166c4e8e23ba4e8d4d1a
gkfktkrh153 <gkfktkrh153@naver.com>
2024-09-15 21:36:13 +0900

    init

---
+++ src/main/java/com/kcc/banking/MicroBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kcc.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBankingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kcc/banking/MicroBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
