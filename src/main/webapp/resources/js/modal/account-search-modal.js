78658bfa78fd573a1fdd77b545e23c3fa208e5ba
221B0825 <eunseoyu0825@gmail.com>
2024-09-25 11:17:27 +0900

    feat: add LIKE AccountMapper

--- src/main/webapp/resources/js/modal/account-search-modal.js
+++ src/main/webapp/resources/js/modal/account-search-modal.js
@@ -1,42 +1,50 @@
 $(document).ready(function() {
     $('#modal-check-account').click(function() {
-        var accountNumber = $('#modal-input-account').val();
-        $.ajax({
-            url: "/api/employee/account",
-            data: { accountNumber: accountNumber, productName: null},
-            type: "GET",
-            success: function(data) {
-                var accountTableBody = $("#modal-common-table tbody");
-                accountTableBody.empty();
-
-                $.each(data, function(index, account) {
-                    var row = "<tr>" +
-                        "<td><button class='button-main '>선택</button></td>" +
-                        "<td>" + account.accountId + "</td>" +
-                        "<td>" + account.openDate + "</td>" +
-                        "<td>" + account.customerName + " 님</td>" +
-                        "<td>" + account.productName + "</td>" +
-                        "<td>" + account.balance.toLocaleString('ko-KR') + " 원</td>" +
-                        "</tr>";
-                    accountTableBody.append(row);
-                });
-            },
-            error: function(error) {
-                console.log("Error while fetching accounts", error);
-            }
-        });
+        checkAccount();  // 계좌 조회 함수 호출
     });
 
     $('#modal-check-account-reset').click(function() {
-        $('#modal-input-account').val('');
-        var accountTableBody = $("#modal-common-table tbody");
-        accountTableBody.empty();
+        resetAccountInput();  // 입력 및 테이블 초기화 함수 호출
     });
 
     // 모달이 닫힐 때 테이블 초기화
     $('#accountSearchModal').on('hide.bs.modal', function() {
-        var accountTableBody = $("#modal-common-table tbody");
-        accountTableBody.empty();
-        $('#modal-input-account').val('');
+        resetAccountInput();  // 입력 및 테이블 초기화 함수 호출
     });
 });
+
+// 계좌 조회 함수
+function checkAccount() {
+    var accountNumber = $('#modal-input-account').val();
+    $.ajax({
+        url: "/api/employee/account",
+        data: { accountNumber: accountNumber, productName: null},
+        type: "GET",
+        success: function(data) {
+            var accountTableBody = $("#modal-common-table tbody");
+            accountTableBody.empty();
+
+            $.each(data, function(index, account) {
+                var row = "<tr>" +
+                    "<td><button class='button-main select-account'>선택</button></td>" +
+                    "<td>" + account.accountId + "</td>" +
+                    "<td>" + account.openDate + "</td>" +
+                    "<td>" + account.customerName + " 님</td>" +
+                    "<td>" + account.productName + "</td>" +
+                    "<td>" + account.balance.toLocaleString('ko-KR') + " 원</td>" +
+                    "</tr>";
+                accountTableBody.append(row);
+            });
+        },
+        error: function(error) {
+            console.log("Error while fetching accounts", error);
+        }
+    });
+}
+
+// 입력 및 테이블 초기화 함수
+function resetAccountInput() {
+    $('#modal-input-account').val('');
+    var accountTableBody = $("#modal-common-table tbody");
+    accountTableBody.empty();
+}
