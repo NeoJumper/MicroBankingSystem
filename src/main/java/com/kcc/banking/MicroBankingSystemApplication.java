40975ece079f9ed55ce8ae6a77882814017d9abb
221B0825 <eunseoyu0825@gmail.com>
2024-09-30 11:49:37 +0900

    feat: auto insert Table AND Data

--- src/main/java/com/kcc/banking/MicroBankingSystemApplication.java
+++ src/main/java/com/kcc/banking/MicroBankingSystemApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicroBankingSystemApplication {
 
 	public static void main(String[] args) {
-
-
-
 		SpringApplication.run(MicroBankingSystemApplication.class, args);
 	}
 
