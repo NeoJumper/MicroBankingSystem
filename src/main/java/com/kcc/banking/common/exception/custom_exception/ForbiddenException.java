ed456e77116d3723e537208d0735d063c5a29fd5
gkfktkrh153 <gkfktkrh153@naver.com>
2024-10-01 10:53:04 +0900

    공통 예외 처리

--- src/main/java/com/kcc/banking/common/exception/custom_exception/ForbiddenException.java
+++ src/main/java/com/kcc/banking/common/exception/custom_exception/ForbiddenException.java
@@ -1,7 +1,7 @@
 package com.kcc.banking.common.exception.custom_exception;
 
-import com.kcc.vacation.global.exception.CustomException;
-import com.kcc.vacation.global.exception.ErrorCode;
+import com.kcc.banking.common.exception.CustomException;
+import com.kcc.banking.common.exception.ErrorCode;
 
 public class ForbiddenException extends CustomException {
     public ForbiddenException(ErrorCode errorCode) {
