5f14804c7455bd7f88070867166258ff7deadda4
kimyoungjin <youngjin1293@naver.com>
2024-10-07 10:54:04 +0900

    account close controller 변경

--- src/main/java/com/kcc/banking/domain/account_close/controller/AccountCloseController.java
+++ src/main/java/com/kcc/banking/domain/account_close/controller/AccountCloseController.java
@@ -3,8 +3,10 @@ package com.kcc.banking.domain.account_close.controller;
 import com.kcc.banking.domain.account_close.service.AccountCloseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,7 +19,9 @@ public class AccountCloseController {
     }
 
     @GetMapping("/page/employee/account-close-cancel")
-    public String accountCloseCancel() {
+    public String accountCloseCancel(Model model, @RequestParam long accId) {
+        model.addAttribute("accId", accId);
+        System.out.println(accId+"================================");
         return "account_close/account-close-cancel";
     }
 }
