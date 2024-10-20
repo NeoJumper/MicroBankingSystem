d838fe9348791619f1c84de5028b5b743e5a1a43
221B0825 <eunseoyu0825@gmail.com>
2024-10-20 19:54:12 +0900

    feat: change branchClosingVaultCash

--- src/main/java/com/kcc/banking/domain/business_day_close/dto/response/ManagerClosingData.java
+++ src/main/java/com/kcc/banking/domain/business_day_close/dto/response/ManagerClosingData.java
@@ -3,12 +3,14 @@ package com.kcc.banking.domain.business_day_close.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class ManagerClosingData {
 
     private List<ClosingData> closingDataList;
@@ -29,13 +31,21 @@ public class ManagerClosingData {
     }
 
     public static ManagerClosingData of(List<ClosingData> closingDataList, BigDecimal branchClosingVaultCash, String branchName){
-        return ManagerClosingData.builder()
+        ManagerClosingData managerClosingData = ManagerClosingData.builder()
                 .closingDataList(closingDataList)
-                .prevCashBalanceOfBranch(closingDataList.stream().map(ClosingData::getPrevCashBalance).reduce(BigDecimal.ZERO, BigDecimal::add) )
+                .prevCashBalanceOfBranch(closingDataList.stream().map(ClosingData::getPrevCashBalance).reduce(BigDecimal.ZERO, BigDecimal::add))
                 .totalDepositOfBranch(closingDataList.stream().map(ClosingData::getTotalDeposit).reduce(BigDecimal.ZERO, BigDecimal::add))
                 .totalWithdrawalOfBranch(closingDataList.stream().map(ClosingData::getTotalWithdrawal).reduce(BigDecimal.ZERO, BigDecimal::add))
-                .vaultCashOfBranch(branchClosingVaultCash)
                 .branchName(branchName)
                 .build();
+
+        // branchClosingVaultCash == null 이라면 아직 OPEN 중인 상태
+        if(branchClosingVaultCash == null) {
+            // 직전 영업일 현금 마감액 + 오늘자 현금 입금액 - 오늘자 현금 출금액
+            BigDecimal expectedBranchClosingVaultCash = managerClosingData.prevCashBalanceOfBranch.add(managerClosingData.totalDepositOfBranch).subtract(managerClosingData.totalWithdrawalOfBranch);
+            managerClosingData.setVaultCashOfBranch(expectedBranchClosingVaultCash);
+        }
+        
+        return managerClosingData;
     }
 }
