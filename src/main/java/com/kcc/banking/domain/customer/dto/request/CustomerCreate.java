c34bd44906dedee179b7d4b85bc13953f25742ca
gkfktkrh153 <gkfktkrh153@naver.com>
2024-10-28 07:51:00 +0900

    고객 등록 기능 구현

--- src/main/java/com/kcc/banking/domain/customer/dto/request/CustomerCreate.java
+++ src/main/java/com/kcc/banking/domain/customer/dto/request/CustomerCreate.java
@@ -1,5 +1,6 @@
 package com.kcc.banking.domain.customer.dto.request;
 
+import com.kcc.banking.domain.common.dto.request.CurrentData;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,17 +14,30 @@ public class CustomerCreate {
     private String birthDate;
     private String name;
     private String phoneNumber;
+    private String address;
+    private String gender;
+    private String detailAddress;
+    private String identificationCode;
     private String email;
     private String registrantId;
 
     @Builder
-    public CustomerCreate(Long id, Long branchId, String birthDate, String name, String phoneNumber, String email, String roles, String registrantId, String registrationDate, Long version) {
+    public CustomerCreate(Long id, Long branchId, String birthDate,String gender, String name, String phoneNumber, String address, String detailAddress, String identificationCode, String email, String registrantId) {
         this.id = id;
         this.branchId = branchId;
         this.birthDate = birthDate;
+        this.gender = gender;
         this.name = name;
         this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.detailAddress = detailAddress;
+        this.identificationCode = identificationCode;
         this.email = email;
         this.registrantId = registrantId;
+    }
+
+    public void setCommonData(CurrentData currentData) {
+        this.branchId = currentData.getBranchId();
+        this.registrantId = String.valueOf(currentData.getEmployeeId());
     }
 }
